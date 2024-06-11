f1d54b6680898a057820bc61247c41f764ba4aa8
Pranaya Joshi WSC <joiship@wsc.school.nz>
2024-06-11 12:26:57 +1200

    intro

---
+++ main.py
@@ -0,0 +1,5 @@
+# INTRODUCTION 
+
+name = input("You're a peasant living in a town on the outskirts of Struht. Opportunities are yet to come for you, adventurer. You constantly dream of being at the top, the strongest, the one to inspire people.  What's your name, Adventurer?")
+print(" Welcome to the world" ,name)
+name = input("What's your name?")
