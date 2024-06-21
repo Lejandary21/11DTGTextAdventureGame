878cf88ed772f142e0e422a8473afa0ae68f02be
Pranaya Joshi WSC <joiship@wsc.school.nz>
2024-06-21 12:57:10 +1200

    push

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def game_intro():
     print("Opportunities are yet to come for you, adventurer.")
     print("You constantly dream of being at the top, the strongest, the one to inspire people.")
     player_name = input("What's your name, adventurer? ")
+    print("You open your eyes. You sit up on your bed and your eyes began to wonder around your surroundings. Your house is dusty, shabby house. Cobwebs linger in the corner of each room, it's been a long time since you last cleaned it. You get up from your bed and decided to start anew and begin your journey.")
+  
 
 
 
